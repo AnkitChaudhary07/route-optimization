fcc50053cd7236ba5f777eaa1261639ea67fdd84
AnkitChaudhary07 <125671143+AnkitChaudhary07@users.noreply.github.com>
2023-02-19 15:21:25 +0530

    Add files via upload

---
+++ Routing/script.js
@@ -0,0 +1,89 @@
+let map = L.map('map', {
+    layers: MQ.mapLayer(),
+    center: [30.260428429550945, 78.09673527551112],
+    zoom: 10
+});
+
+function runDirection(start, end) {
+        
+    // recreating new map layer after removal
+    map = L.map('map', {
+        layers: MQ.mapLayer(),
+        center: [30.260428429550945, 78.09673527551112],
+        zoom: 10
+    });
+    
+    var dir = MQ.routing.directions();
+
+    dir.route({
+        locations: [
+            start,
+            end
+        ]
+    });
+
+
+    CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+        createStartMarker: (location) => {
+            var custom_icon;
+            var marker;
+
+            custom_icon = L.icon({
+                iconUrl: 'img/red.svg',
+                iconSize: [20, 29],
+                iconAnchor: [10, 29],
+                popupAnchor: [0, -29]
+            });
+
+            marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+            return marker;
+        },
+
+        createEndMarker: (location) => {
+            var custom_icon;
+            var marker;
+
+            custom_icon = L.icon({
+                iconUrl: 'img/blue.svg',
+                iconSize: [20, 29],
+                iconAnchor: [10, 29],
+                popupAnchor: [0, -29]
+            });
+
+            marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+            return marker;
+        }
+    });
+    
+    map.addLayer(new CustomRouteLayer({
+        directions: dir,
+        fitBounds: true
+    })); 
+}
+
+
+// function that runs when form submitted
+function submitForm(event) {
+event.preventDefault();
+
+// delete current map layer
+map.remove();
+
+// getting form data
+start = document.getElementById("start").value;
+end = document.getElementById("destination").value;
+
+// run directions function
+runDirection(start, end);
+
+// reset form
+document.getElementById("form").reset();
+}
+
+// asign the form to form variable
+const form = document.getElementById('form');
+
+// call the submitForm() function when submitting the form
+form.addEventListener('submit', submitForm);
